2d3171b344fb0f1aa843991027dd38ca17b82f83
doxxapapa <36539192+doxxapapa@users.noreply.github.com>
2020-04-22 13:52:38 +0200

    Update CalculatorImplTest.java
    1.0

--- calculator/calculator-cli/src/test/java/org/example/calculator/cli/CalculatorImplTest.java
+++ calculator/calculator-cli/src/test/java/org/example/calculator/cli/CalculatorImplTest.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package org.example.calculator.cli;
 import static jdk.nashorn.internal.objects.Global.Infinity;
 import static org.hamcrest.CoreMatchers.equalTo;
@@ -39,4 +40,47 @@ class CalculatorImplTest {
         assertThat(Infinity, equalTo(cl.divide(1.0, 0.0)));
 
     }
+=======
+package org.example.calculator.cli;
+import static jdk.nashorn.internal.objects.Global.Infinity;
+import static org.hamcrest.CoreMatchers.equalTo;
+import org.junit.jupiter.api.Test;
+import static org.junit.Assert.assertThat;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class CalculatorImplTest {
+
+    @Test
+    void add() {
+        CalculatorImpl cl = new CalculatorImpl();
+        assertThat(3.0, equalTo(cl.add(1.0, 2.0)));
+        assertThat(-10.0, equalTo(cl.add(-5.0, -5.0)));
+    }
+
+    @Test
+    void multiply() {
+        CalculatorImpl cl = new CalculatorImpl();
+        assertThat(0.0, equalTo(cl.multiply(0.0, 1.0)));
+        assertThat(-10.0, equalTo(cl.multiply(2.0, -5.0)));
+        assertThat(10.0, equalTo(cl.multiply(2.0, 5.0)));
+    }
+
+    @Test
+    void substract() {
+        CalculatorImpl cl = new CalculatorImpl();
+        assertThat(-1.0, equalTo(cl.substract(1.0, 2.0)));
+        assertThat(0.0, equalTo(cl.substract(-5.0, -5.0)));
+    }
+
+    @Test
+    void divide() {
+        CalculatorImpl cl = new CalculatorImpl();
+        assertThat(3.0, equalTo(cl.divide(9.0, 3.0)));
+        assertThat(0.0, equalTo(cl.divide(0.0, 2.0)));
+        assertThat(Infinity, equalTo(cl.divide(1.0, 0.0)));
+
+    }
+>>>>>>> 80c05a851fa8c12797cff7553d1371e0f5e1aecc
 }
